2e7d9f153abceda1a5a736b1137d25a638dc58bc
uddivert <uddhavswami@gmail.com>
2022-09-26 12:20:38 -0400

    created NodeType.java

--- src/NodeType.java
+++ src/NodeType.java
@@ -1,5 +1,19 @@
 public class NodeType {
     public ItemType info;
     public NodeType next;
+
+    /**
+     * Constructor for NodeType
+     */
+    public NodeType(ItemType item) {
+        item = info;
+    }
+
+    /**
+     * This will set the next Node
+     */
+    public next(NodeType nextOne) {
+        nextOne = next;
+    }// next
 }
 
